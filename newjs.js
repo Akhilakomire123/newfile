c0e77af2e6f97671496dda25adf71938916e5ba4
Akhilakomire123 <akhilakomire.222@gmail.com>
2022-11-03 22:38:46 +0530

    adding newjs

---
+++ newjs.js
@@ -0,0 +1,19 @@
+
+
+let headTitle=document.getElementById("header-title");
+
+console.log(headTitle);
+
+headTitle.textContent="WelCome";
+
+let mainHeader=document.getElementById("main-header");
+
+mainHeader.style.borderBottom='solid 3px #000';
+
+let items=document.getElementByclassName("list-group");
+
+items[3].style.backgroundcolor='green';
+
+items.style.fontWeight='bold';
+
+console.log(items);
